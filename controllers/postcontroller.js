20a03bc106fd7576ad1762d6e35d639f24bd946c
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-14 15:27:53 +0100

    Refactor post controller and routes to implement CRUD operations

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -2,20 +2,21 @@ function index(req, res) {
     res.json(post)
 }
 function show(req, res) {
+    res.json(post[req.params.id])
+}
+function create(req, res) {
+    res.send('Aggiunta di un nuovo post');
+}
+function edit(req, res) {
+    res.send(`Modifica di un post esistente con id ${req.params.id}`);
 
 }
-function index(req, res) {
-
-}
-function store(req, res) {
-
-}
-function modify(req, res) {
+function update(req, res) {
+    res.send(`Aggiornamento di una parte del post esistente con id ${req.params.id}`);
 
 }
 function destroy(req, res) {
+    res.send(`Cancellazione di un post esistente con id ${req.params.id}`);
 
 }
-function index(req, res) {
-
-} 
+module.exports = ({ index, show, create, edit, update, destroy });
