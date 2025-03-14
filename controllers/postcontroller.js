5c9432e3e69bc8b5eb3b99a66e1631f122fd1dc0
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-14 15:21:07 +0100

    Initialize Express server and set up basic post routes with sample data

---
+++ controllers/postcontroller.js
@@ -0,0 +1,21 @@
+function index(req, res) {
+    res.json(post)
+}
+function show(req, res) {
+
+}
+function index(req, res) {
+
+}
+function store(req, res) {
+
+}
+function modify(req, res) {
+
+}
+function destroy(req, res) {
+
+}
+function index(req, res) {
+
+} 
---
+++ routes/posts.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const post = require('../data/posts');
+
+router.get('/', index)
+router.get('/:id', (req, res) => {
+    /* res.send(`Post con id ${req.params.id}`) */
+    res.json(post[req.params.id])
+})
+router.post('/', (req, res) => {
+    // Aggiunta di un nuovo post
+    res.send('Aggiunta di un nuovo post');
+})
+router.put('/:id', (req, res) => {
+    // Modifica di un post esistente
+    res.send(`Modifica di un post esistente con id ${req.params.id}`);
+})
+router.patch('/:id', (req, res) => {
+    // Aggiornamento di una parte del post esistente
+    res.send(`Aggiornamento di una parte del post esistente con id ${req.params.id}`);
+})
+
+
+router.delete('/:id', (req, res) => {
+    // Cancellazione di un post esistente
+    res.send(`Cancellazione di un post esistente con id ${req.params.id}`);
+})
+module.exports = router
