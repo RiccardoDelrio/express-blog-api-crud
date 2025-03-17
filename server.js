c446a369a1aab68aaf95661b23d1d3c4aa30e0c8
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-17 12:10:59 +0100

    Add JSON parsing middleware and integrate post routes in server setup

--- server.js
+++ server.js
@@ -6,5 +6,7 @@ app.listen(port, () => {
 
     console.log(`Server attivo http://localhost:${port}`)
 })
+app.use(express.json());
+
 const postsRoutes = require('./routes/posts') //IMPORTO LE ROTTE PER I POST
 app.use('/posts', postsRoutes) //AGGIUNGO LE ROTTE AL SERVER
