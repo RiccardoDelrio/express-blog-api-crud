e28dbfb27e2aa3b7b7538a05d8190f3b4b456078
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-18 12:59:11 +0100

    Fix import path for error handling middleware in server configuration

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000
 app.use(express.json());
 
 const postsRoutes = require('./routes/posts') //IMPORTO LE ROTTE PER I POST
-const handlerError = require('./middleware/handlerErrors') //')
+const handlerError = require('./middleware/handlerError') //')
 const error_404 = require('./middleware/error_404')
 app.use('/posts', postsRoutes) //AGGIUNGO LE ROTTE AL SERVER
 app.use(handlerError)
