5b8e9bf386ae951535e7744a0c30fc0a97e3ec9b
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-18 12:56:24 +0100

    Add error handling middleware for 500 responses and integrate 404 error handling

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+
+app.use(express.json());
+
+const postsRoutes = require('./routes/posts') //IMPORTO LE ROTTE PER I POST
+const handlerError = require('./middleware/handlerErrors') //')
+const error_404 = require('./middleware/error_404')
+app.use('/posts', postsRoutes) //AGGIUNGO LE ROTTE AL SERVER
+app.use(handlerError)
 app.listen(port, () => {
 
     console.log(`Server attivo http://localhost:${port}`)
 })
-app.use(express.json());
-
-const postsRoutes = require('./routes/posts') //IMPORTO LE ROTTE PER I POST
-app.use('/posts', postsRoutes) //AGGIUNGO LE ROTTE AL SERVER
+app.use(error_404) //aggiunta middleware per gestire errori 404
